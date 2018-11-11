3850ad8653d323813572af5bf55368c368fc23e3
Daniel Grinshpon <GrinshponDaniel@protonmail.com>
2018-11-10 20:53:47 -0500

    Update markovFront.hs

--- markovFront.hs
+++ markovFront.hs
@@ -1,10 +1,21 @@
 -- Markov: Haskell Powered Calculator
 --Daniel Grinshpon
+import System.IO
+import System.Exit
 import Markov
 
-input :: IO ()
-input = do
-    let inp = ">> "
-    print "WIP"
+input :: [Char] -> IO ()
+input " " = do
+    putStrLn "-----------------------------\n|-----Markov Calculator-----|\n-----------------------------"
+    input "> "
+input x = do
+    putStr "> "
+    hFlush stdout
+    inpt <- getLine
+    let out = output inpt
+    putStrLn out
+    if out == "Quit"
+        then exitSuccess
+    else input out --eventually the previous 'out' will be able to be used as an ANS in a next expression
 
-main = print $ output "2+2"
+main = input " "
